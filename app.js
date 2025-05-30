8e59c151b1b3bc05be675717388960d3e5cd014b
Bhawuk Arora <bhawukarora042@gmail.com>
2025-05-30 12:04:19 +0530

    node basics completed

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-const catMe = require('http')
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+
+
+app.get('/', (req, res) => {
+    res.end("Home Page!")
+})
+
+app.get('/about', (req, res) => {
+    res.end("About Page!")
+})
+
+
+app.listen(3001)
---
+++ temp.js
@@ -0,0 +1,14 @@
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//     if (req.url == "/about") {
+//         res.end("About Page!");
+//     } else if (req.url == "/home") {
+//         res.end("Home Page!");
+//     } else {
+//         res.end("Hello Bhawuk!");
+//     }
+// });
+
+// server.listen(3000)
+
